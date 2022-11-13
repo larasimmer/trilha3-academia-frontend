0df08900896f507c1321b6aa51c09570434f00a4
WBJ-000159 <larafsimmer@gmail.com>
2022-11-13 14:58:19 -0300

    Creating frontend theme

---
+++ app/design/frontend/Webjump/trilha-3/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Webjump/trilha-3', __DIR__);
